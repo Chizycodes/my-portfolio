d5a5548646f69f994f6dfc3b03f801c7abf81a3e
Chizycodes <okwudirejoy@gmail.com>
2023-12-03 16:07:55 +0100

    Initial setup and navigation

---
+++ src/components/nav/nav-utils.ts
@@ -0,0 +1,22 @@
+export const navOptions = [
+	{
+		name: 'About',
+		href: 'about',
+	},
+	{
+		name: 'Work',
+		href: 'work',
+	},
+	{
+		name: 'Skills',
+		href: 'skills',
+	},
+	{
+		name: 'Blog',
+		href: 'blog',
+	},
+	{
+		name: 'Contact',
+		href: 'contact',
+	},
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				neon: '#52DEE5',
+				text: '#C7D3FF',
+				textDark: '#A1ABBF',
+				bgblue: '#222E50',
+				darkerBlue: '#0A192F',
+			},
+			fontFamily: {
+				sora: ['Sora', 'sans-serif'],
+				fira: ['Fira Code', 'monospace'],
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
