7a875615a7256d97ceb49c27b8cf85703598d82c
Chizycodes <okwudirejoy@gmail.com>
2023-12-04 10:51:39 +0100

    About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+'use client';
 import { About } from '@/components/about';
 import { Intro } from '@/components/intro/Intro';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function Home() {
+	useEffect(() => {
+		AOS.init();
+	}, []);
 	return (
 		<>
 			<Intro />
