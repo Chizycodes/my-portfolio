e84a875fa0a2e812d510d79000f9563e1c2b315b
Chizycodes <okwudirejoy@gmail.com>
2023-12-04 01:10:59 +0100

    intro section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import Header from '@/components/nav/Header';
 import SideBar from '@/components/nav/Sidebar';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -14,10 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body>
 				<Header />
 				<SideBar />
-				<div className="flex flex-col items-center justify-center mt-40 space-y-10 md:justify-start md:items-start md:ml-20 ml-10 mb-10">
+				<div className="flex flex-col items-center justify-center mt-40 space-y-10 md:justify-start md:items-start mb-10 w-full">
+					<div className="w-64 h-64 rounded-full bg-neon fixed mx-auto my-auto blur-xl inset-0 opacity-20" />
 					{children}
 				</div>
 			</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import { Intro } from '@/components/intro/Intro';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="max-w-5xl w-full"></div>
-		</main>
+		<>
+			<Intro />
+		</>
 	);
 }
