64f6da343486ddb5b0dacebeb98fcf31cc44cc8e
Chizycodes <okwudirejoy@gmail.com>
2023-12-07 17:08:23 +0100

    project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,4 @@
+import Heading from '@/components/projects/Heading';
 import ProjectCard from '@/components/projects/ProjectCard';
 import data from '@/data';
 import React from 'react';
@@ -6,7 +7,7 @@ const Projects = () => {
 	const projectsList = data?.projects;
 	return (
 		<div className="flex flex-col items-center justify-center max-w-7xl w-full mx-auto p-5 md:p-10">
-			<h1 className='text-text'>Projects</h1>
+			<Heading />
 			<div className="grid grid-cols-1 gap-8 md:grid-cols-3 items-start">
 				{projectsList.map((item) => {
 					return <ProjectCard key={item.id} project={item} />;
--- src/components/projects/Heading.tsx
+++ src/components/projects/Heading.tsx
@@ -10,7 +10,7 @@ function Heading({ tag }: HeadingProps) {
 		<div className={`${tag ? 'pt-10 pb-4 sm:pt-24 sm:pb-20' : 'py-12 sm:py-20'} w-full text-center relative`}>
 			{tag ? (
 				<>
-					<h1 className="text-3xl sm:text-4xl inline-block w-auto mx-auto mb-8 relative">
+					<h1 className="text-3xl sm:text-4xl inline-block w-auto mx-auto mb-8 relative text-text">
 						Projects built with <b>{tag}</b>
 						<Image
 							width={300}
@@ -22,15 +22,12 @@ function Heading({ tag }: HeadingProps) {
 					</h1>
 				</>
 			) : (
-				<h1 className="text-4xl sm:text-6xl inline-block w-auto mx-auto mb-8 relative">
-					Projects
-					<Image className="sqD w-10 -top-8 -right-8 absolute" src="/static/doodles/hero/code.svg" />
-				</h1>
+				<h1 className="text-4xl sm:text-6xl inline-block w-auto mx-auto mb-8 relative text-text">Projects</h1>
 			)}
 			{!tag && (
-				<p className="text-fun-gray text-xl sm:text-2xl max-w-3xl m-auto">
-					I've built cool apps and websites using anything from HTML to React (and even PHP!). Here are some of my
-					favorite projects over the course of my journey.
+				<p className="text-xl max-w-3xl m-auto text-textDark">
+					I&apos;ve built cool apps using a variety of technologies, from React/Next.js on the frontend to Node.js on
+					the backend. Here are some of my favorites.
 				</p>
 			)}
 		</div>
